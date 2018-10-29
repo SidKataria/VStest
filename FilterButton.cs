aef5f6f77406077723c9c7eb58afe8863c627a15
Siddharth Kataria <32744185+SidKataria@users.noreply.github.com>
2018-10-28 19:20:40 -0600

    Add files via upload

---
+++ FilterButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace realestatefinder
+{
+    public class FilterButton : Button
+    {
+        static FilterButton()
+        {
+        }
+
+        public void Handle_FilterButton_Click()
+        {
+            this.Background = this.Background == Brushes.AliceBlue ? Brushes.SlateBlue : Brushes.AliceBlue;
+        }
+    }
+}
